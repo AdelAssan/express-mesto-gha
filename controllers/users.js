9cfc3ac2802dd1f03a12beb963da29132c244f74
AdelAssan <adelassanova@gmail.com>
2022-05-23 19:20:19 +0600

    bd13: first try

---
+++ controllers/users.js
@@ -0,0 +1,74 @@
+const User = require('../models/user');
+
+module.exports.postUser = (req, res) => {
+    const {name, about, avatar} = req.body;
+    return User.create({name, about, avatar})
+        .then((user) => res.send({data: user}))
+        .catch((error) => {
+            if (error.name === 'ValidationError') {
+                res.status(400).send({message: "Переданы некорректные данные"});
+                return;
+            }
+            res.status(500).send({message: "Ошибка по умолчанию."});
+        });
+}
+
+module.exports.getUsers = (req, res) => {
+    User.find({})
+        .then((user) => res.send({data: user}))
+        .catch(() => res.status(500).send({message: "Ошибка по умолчанию"}));
+}
+
+module.exports.searchUser = (req, res) => {
+    User.findById(req.params.userId)
+        .then((user) => {
+            if (!user) {
+                return res.status(404).send({message: "Пользователь не найден"});
+            }
+            return res.send({data: user});
+        })
+        .catch((error) => {
+            if (error.name === 'CastError') {
+                return res.status(400).send({message: "Переданы некорректные данные"});
+            }
+            return res.status(500).send({message: "Ошибка по умолчанию"});
+        });
+}
+
+module.exports.updateProfile = (req, res) => {
+    const {name, about} = req.body;
+    User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
+        .then((user) => {
+            if(!user) {
+                res.status(404).send({message: "Пользователь не найден"});
+                return;
+            }
+            res.send({data: user});
+        })
+        .catch((error) => {
+            if(error.name === 'ValidatorError') {
+                res.status(400).send({message: "Переданы некорректные данные"});
+                return;
+            }
+            res.status(500).send({message: "Ошибка по умолчанию"});
+        });
+}
+
+module.exports.updateAvatar = (req,res) => {
+    const {avatar} = req.body;
+    User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
+        .then((user) => {
+            if(!user) {
+                res.status(404).send({message: "Пользователь не найден"});
+                return;
+            }
+            res.send({data: user});
+        })
+        .catch((error) => {
+            if(error.name === 'ValidatorError') {
+                res.status(400).send({message: "Переданы некорректные данные"});
+                return;
+            }
+            res.status(500).send({message: "Ошибка по умолчанию"});
+        });
+}
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+    postUser, getUsers, searchUser, updateProfile, updateAvatar
+} = require('../controllers/users');
+
+router.get('/users', getUsers);
+router.get('/users/:userId', searchUser);
+router.post('/users', postUser);
+router.patch('/users/me', updateProfile);
+router.patch('/users/me/avatar', updateAvatar);
+
+module.exports = router;
