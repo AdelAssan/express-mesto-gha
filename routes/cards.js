c4c93c55b9755e616ecdb667200589c68629e7c8
AdelAssan <adelassanova@gmail.com>
2022-06-06 18:22:45 +0600

    bd14: first try

--- routes/cards.js
+++ routes/cards.js
@@ -6,7 +6,12 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
-router.post('/cards', postCard);
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}), postCard);
 router.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().alphanum().length(24),
